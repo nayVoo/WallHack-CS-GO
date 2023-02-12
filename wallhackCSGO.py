50f92d1eac76cf779c9b06c4f64edecf3480dd49
nayVoo <124888494+nayVoo@users.noreply.github.com>
2023-02-12 19:44:52 +0200

    Update wallhackCSGO.py

--- wallhackCSGO.py
+++ wallhackCSGO.py
@@ -1,24 +1,24 @@
 import pymem
 import keyboard
-
-dwEntityList = 0x4DFFF04
-dwGlowObjectManager = 0x535A9D8
-m_iGlowIndex = 0x10488
+#OFFSETS 
+dwEntityList = () #--> dwEntityList offset
+dwGlowObjectManager = () #--> dwGlowObjectManager offset
+m_iGlowIndex = () #--> m_iGlowIndex offset
 
 pm = pymem.Pymem('csgo.exe')
 clientcatch = pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
 
-isclicker = False
+isclicker = False 
 
 def set_clicker():
-	global isclicker
+	global isclicker # global allows us to manage isclicker func even in other program
 	if isclicker:
 		isclicker = False
 		print ('Wh on')
 	else:
 		isclicker = True
-		print ('Wh off')
-keyboard.add_hotkey ('Alt + Z', set_clicker)
+		print ('')
+keyboard.add_hotkey ('Alt + Z', set_clicker) # Bind Alt + Z --> turn on, turn off WH (you can use any bund u want)
 
 def main():
 	while True:
@@ -37,9 +37,9 @@ def main():
 					entityglowing = pm.read_int(entity+m_iGlowIndex)
 
 					pm.write_float(glow + entityglowing * 0x38 + 0x8, float(0))
-					pm.write_float(glow + entityglowing * 0x38 + 0xC, float(1))
+					pm.write_float(glow + entityglowing * 0x38 + 0xC, float(1)) # --> color (1 - green, 2- blue, 3 - red)
 					pm.write_float(glow + entityglowing * 0x38 + 0x10, float(0))
 					pm.write_float(glow + entityglowing * 0x38 + 0x14, float(1))
-					pm.write_int(glow + entityglowing * 0x38 + 0x28, 1)
+					pm.write_int(glow + entityglowing * 0x38 + 0x28, 1) # --> alpha
 if __name__ == '__main__':
 	main()
